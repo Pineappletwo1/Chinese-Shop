0eecb593ebc205e3932e7f8eb537c278dd3527fa
TheLazyCodernothacker <120674735+TheLazyCodernothacker@users.noreply.github.com>
2024-04-05 08:35:35 -0700

    Update page.tsx

--- chinese-shop/src/app/shop/page.tsx
+++ chinese-shop/src/app/shop/page.tsx
@@ -92,7 +92,7 @@ export default function Shop() {
         </Link>
         <Link href="/checkout">
           <h1 className="text-xl hover:bg-gray-300 rounded p-2 ">
-            {isOn ? "Checkout" : "查看"}
+            {isOn ? "Checkout" : "结帐"}
           </h1>
         </Link>
       </nav>
@@ -151,7 +151,7 @@ export default function Shop() {
         </ul>
         <Link href="/checkout">
           <button className="bg-green-500 text-white px-4 py-2 rounded mt-4 text-xl">
-            {isOn ? "Checkout" : "查看"}
+            {isOn ? "Checkout" : "结帐"}
           </button>
         </Link>
       </div>
